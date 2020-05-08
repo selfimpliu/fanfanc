f60963066d880296eee1095ea2d5a24ef5d0a146
dafanfan <418796187@qq.com>
2020-05-08 23:24:37 +0800

    price

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+
+
+      int a;
+      a=1;
+      printf("hello world");
+      printf("computer.\n");
+      printf("%d\n",a);
+ 
+
+
+      return 0;
+}
---
+++ worth.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+       float weight;
+       float unit_price;
+       weight=300.68;
+       unit_price=6.99;
+      
+       printf("%f %f %f.\n", weight, unit_price, weight* unit_price);
+
+      return 0;
+}
+       
